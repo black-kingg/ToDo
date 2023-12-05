33dcd780abedfd4c51a3ae4d83932f0fd88730fd
Mubarak <mudet04@gmail.com>
2023-12-05 06:03:57 +0100

    logOut added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,29 @@
 import React, { useState } from "react";
+import { getAuth, signOut } from "firebase/auth";
 
 const Navbar = () => {
+  const handleLogout = () => {
+    const auth = getAuth();
+
+    signOut(auth)
+      .then(() => {
+        console.log("User signed out successfully");
+      })
+      .catch((error) => {
+        console.error("Error signing out:", error.message);
+      });
+  };
+
   return (
-    <div className=" w-screen bg-cyan-200 flex justify-between items-center pt-8 pb-4 px-80 max-2xl:px-16 max-xl:px-8 max-md:px-8">
-      <div className="flex ">
-        <div className=" flex items-center">
-          <div className="flex">
-            <p className="text-3xl ">TODO</p>
-          </div>
-        </div>
+    <div className=" w-screen bg-cyan-200 items-center pt-8 pb-4 px-80 max-2xl:px-16 max-xl:px-8 max-md:px-8">
+      <div className=" flex justify-between ">
+        <p className="text-3xl ">TODO</p>
+        <button
+          onClick={handleLogout}
+          className=" bg-green-500 text-white py-2 px-4 rounded"
+        >
+          Log Out
+        </button>
       </div>
     </div>
   );
