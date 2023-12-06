1dd2bc6008c4313715fa36cd894f62c391e8183d
Mubarak <mudet04@gmail.com>
2023-12-06 15:37:32 +0100

    #fixAll

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 import Navbar from "../Components/Navbar";
 import ToDoList from "../Components/ToDoList";
 
+import { firebaseConfig } from "../Script";
+
 function Home() {
+  const app = initializeApp(firebaseConfig);
+  const navigate = useNavigate();
+  const [todoList, setTodoList] = useState([]);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth(app);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+
+      if (user) {
+        fetchData(user.uid);
+      } else {
+        navigate("/");
+      }
+    });
+
+    return () => unsubscribe();
+  }, [app]);
+
   return (
     <>
       <Navbar />
